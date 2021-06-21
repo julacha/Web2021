933e0a48e576522de8dc4d880562bf7cf68267a0
Julia.Zorina <julia.zorinaa@gmail.com>
2021-06-21 22:32:53 +0300

    added vue

--- login.php
+++ login.php
@@ -2,28 +2,24 @@
 
 include('assets/config/db_conn.php');
 
-$req = json_decode(file_get_contents("php://input"), true);
-$email = $req['email'];
 
-function sendjson($response)
-{
-    header('Content-Type: application/json');
-    echo json_encode($response);
-    exit();
-}
+$post = $_POST;
+if (isset($post['login'])) {
+    // $email = $post['email'];
 
-if (empty($email)) {
-    sendjson(['message' => 'Email address is required', 'error' => true]);
-} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    sendjson(['message' => 'Email address is not valid', 'error' => true]);
-}
+    if (empty($post['email'])) {
+        echo 'Email address is required';
+        exit();
+    } else
+        $email = $post['email'];
 
-$query = "SELECT * FROM users WHERE  email = '$email'";
-$result = mysqli_query($conn, $query);
-$count = mysqli_num_rows($result);
+    $query = "SELECT * FROM users WHERE  email = '$email'";
+    $result = mysqli_query($conn, $query);
+    $count = mysqli_num_rows($result);
 
-if ($count > 0) {
-    sendjson(['message' => 'Login Successful']);
-} else {
-    sendjson(['message' => 'Login Not Successful', 'error' => true]);
+    if ($count > 0) {
+        echo "Login Successful";
+    } else {
+        echo "Login Not Successful";
+    }
 }
--- script.js
+++ script.js
@@ -1,75 +1,55 @@
 let app = new Vue({
-  el: "#outer-container",
-  data: {
-    email: "",
-    submit: "",
-    visible:true,
-    errors:[],
-  },
-  methods:{
-    hidden(){
-      this.visible = false;
+    el: "#outer-container",
+    data: {
+      email: "",
+      submit: "",
+      visible:true,
+      errors:[],
     },
-    checkForm: function (e) {
-      this.errors = [];
-
-      if (!this.email) {
-        this.errors.push('Email address is required');
-      } else if (!this.validEmail(this.email)) {
-        this.errors.push('Please provide a valid e-mail address');
+    methods:{
+      hidden(){
+        this.visible = false;
+      },
       }
-
-      if (!this.errors.length) {
-        return true;
+    });
+  
+  
+  function request(e) {
+    e.preventDefault();
+  
+    app.submit = "success";
+    appmob.submit = "success";
+    let method = this.getAttribute("method"),
+      action = this.getAttribute("action"),
+      data = [];
+  
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        console.log("success");
       }
-
-      e.preventDefault();
-    },
-    validEmail: function (email) {
-      var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(email);
+    };
+    xhttp.open(method, action, true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  
+    for (let inp of this.querySelectorAll(
+      "[type=text], [type=checkbox]:checked, textarea, select"
+    )) {
+      data.push(inp.getAttribute("name") + "=" + inp.value);
     }
-    }
-  });
-
-function login(e){
-  e.preventDefault();
-  const form = document.getElementById('formsubscribe');
-  postData('login.php', {email: form.email.value}).then(res => {
-    const span = document.getElementById('error');
-    if (res.error){
-      span.innerText = res.message;
-    }
-  });
-}
-async function postData(url = '', data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-
-let appmob= new Vue({
-  el: "#mobile-container",
-  data: {
-    email: "",
-    submit: "",
-    visible:true
-  },
-  methods:{
-    hidden(){
-      this.visible = false;
-    }
+    xhttp.send(data.join("&"));
   }
-});
+  
+  let appmob= new Vue({
+    el: "#mobile-container",
+    data: {
+      email: "",
+      submit: "",
+      visible:true
+    },
+    methods:{
+      hidden(){
+        this.visible = false;
+      }
+    }
+  });
